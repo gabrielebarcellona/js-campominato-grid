8333f3b8b37ae753da6115f41765d6afcb5d241a
gabrielebarcellona <gabrerozabarcy5@gmail.com>
2023-04-26 10:23:13 +0200

    add script and html

--- script.js
+++ script.js
@@ -5,9 +5,30 @@ Ci saranno quindi 10 caselle per ognuna delle 10 righe.*/
 const eleGrid = document.querySelector('.grid');
 const btnPlay = document.querySelector('#play');
 const selectLevel = document.querySelector('#level');
+const eleRequest = document.querySelector('.request')
 
 btnPlay.addEventListener('click',function(){
+    eleRequest.classList.add('hidden');
+    eleGrid.classList.add('hidden');
 
+    const cell = parseInt(selectLevel.value)
 
-    createGrid(64,eleGrid)
+    createGrid(cell,eleGrid)
 });
+
+function createGrid(cell,eleContainer){
+   
+eleContainer.innerHTML = '';
+
+    for (let i = 1 ; i <= ; i++){
+        const eleCell = document.createElement('div');
+        eleCell.innerHTML = i;
+        eleCell.classList.add('cell');
+        eleContainer.append(eleCell);
+
+        eleCell.addEventListener('click',function(){
+            eleCell.classList.toggle('clicked');
+        });
+    }
+
+}
